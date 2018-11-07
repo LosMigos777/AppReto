7129740ff2bd0ccff3634f0563f81c85168309ee
Jan <janivlog@gmail.com>
2018-11-07 10:20:29 +0100

    x

--- juegalmi/www/js/controllers.js
+++ juegalmi/www/js/controllers.js
@@ -13,11 +13,11 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
- 
+
 }])
 
 .controller('serviciosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
+// You can include any angular dependencies  as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
