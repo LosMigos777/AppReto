e2f536032483685927150fdb2fed2d373eced4f0
Jan <janivlog@gmail.com>
2018-11-08 10:37:44 +0100

    carga el mapa pero las dimensiones no

--- juegalmi/www/js/controllers.js
+++ juegalmi/www/js/controllers.js
@@ -44,36 +44,34 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-  
+
 }])
 
 .controller('localizacionCtrl', ['$scope', '$stateParams', '$ionicLoading', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $ionicLoading) {
-  google.maps.event.addDomListener(window, 'load', function() {
-          var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+    console.log("llega");
+    var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
 
-          var mapOptions = {
-              center: myLatlng,
-              zoom: 11,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-          };
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 11,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-          var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-          navigator.geolocation.getCurrentPosition(function(pos) {
-              map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-              var myLocation = new google.maps.Marker({
-                  position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                  map: map,
-                  title: "My Location"
-              });
-          });
+    navigator.geolocation.getCurrentPosition(function(pos) {
+        map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+        var myLocation = new google.maps.Marker({
+            position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+            map: map,
+            title: "My Location"
+        });
+    });
 
-          $scope.map = map;
-      });
-
+    $scope.map = map;
 }])
 
 .controller('atencionAlClienteCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
